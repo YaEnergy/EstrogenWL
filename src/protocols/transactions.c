56f0424e995c9d8042907e9d60fa444762faf005
YaEnergy <kiano.van.cauter@gmail.com>
2025-05-22 18:09:54 +0200

    Implement transactions

---
+++ src/protocols/transactions.c
@@ -0,0 +1,48 @@
+#include "protocols/transactions.h"
+
+#include <stdlib.h>
+
+#include <wayland-server-core.h>
+#include <wayland-util.h>
+
+#include "util/wl_macros.h"
+
+// Init transaction session.
+void e_trans_session_init(struct e_trans_session* session)
+{
+    wl_list_init(&session->operations);
+}
+
+// Creates a new operation for current transaction in session.
+// Returns NULL on fail.
+struct e_trans_op* e_trans_session_add_op(struct e_trans_session* session, void* src, uint32_t type, void* data)
+{
+    if (session == NULL)
+        return NULL;
+
+    struct e_trans_op* op = calloc(1, sizeof(*op));
+
+    if (op == NULL)
+        return NULL;
+
+    op->type = type;
+    op->src = src;
+    op->data = data;
+
+    wl_signal_init(&op->destroy);
+
+    wl_list_append(session->operations, &op->link);
+
+    return op;
+}
+
+// Frees e_trans_op and emits the destroy signal. To destroy data, use the destroy signal.
+void e_trans_op_destroy(struct e_trans_op* operation)
+{
+    if (operation == NULL)
+        return;
+
+    wl_signal_emit_mutable(&operation->destroy, NULL);
+
+    free(operation);
+}
