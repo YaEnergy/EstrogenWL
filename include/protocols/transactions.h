fd7ca6e8d0a7d83319a78ac1376fbba96144bf83
Kiara <kiano.van.cauter@gmail.com>
2025-05-22 16:53:49 +0000

    Add transactions for later protocol implementations (#1)
    * Add main part of transaction header
    
    * Add transaction header comments
    
    * Add transaction session for each safe macro
    
    * Implement transactions
    
    * Remove trans op from trans session on destroy
    
    * Correct destroy trans op comment in header
    
    * Add clearing of ops in trans sessions

---
+++ include/protocols/transactions.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <wayland-server-core.h>
+#include <wayland-util.h>
+
+// Allows a bunch of operations to be requested and done handled all at once, atomically.
+//TODO: currently only for protocol implementations, but might be useful for layout tiling updates too?
+
+// Keeps a list of transaction operations to be handled all at once when transaction is finished.
+struct e_trans_session
+{
+    struct wl_list operations; //struct e_trans_op*
+};
+
+// An operation to be performed when transaction is finished.
+struct e_trans_op
+{
+    uint32_t type; //operation type
+
+    void* src;
+    void* data;
+
+    // Fired when transaction operation is destroyed. Used to destroy data.
+    struct wl_signal destroy;
+
+    struct wl_list link; //e_trans_session::operations
+};
+
+// Allows looping over operations inside current transaction of session, and destroying the current operation once finished.
+#define e_trans_session_for_each_safe(pos, session) struct e_trans_op* tmp; \
+wl_list_for_each_safe(pos, tmp, &session->operations, link)
+
+// Init transaction session.
+void e_trans_session_init(struct e_trans_session* session);
+
+// Creates a new operation for current transaction in session.
+// Returns NULL on fail.
+struct e_trans_op* e_trans_session_add_op(struct e_trans_session* session, void* src, uint32_t type, void* data);
+
+// Destroy all current operations inside session, basically starting a new transaction.
+void e_trans_session_clear(struct e_trans_session* session);
+
+// Frees e_trans_op and emits the destroy signal. To destroy data, use the destroy signal.
+void e_trans_op_destroy(struct e_trans_op* operation);
