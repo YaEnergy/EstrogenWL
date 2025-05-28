c37c87920fd1526de062320ef098261760402602
YaEnergy <kiano.van.cauter@gmail.com>
2025-05-28 14:11:02 +0200

    Remove workspace resource destroy TODO

--- src/protocols/cosmic-workspace-v1.c
+++ src/protocols/cosmic-workspace-v1.c
@@ -66,8 +66,6 @@ static const struct zcosmic_workspace_handle_v1_interface workspace_interface = 
 
 static void e_cosmic_workspace_v1_resource_destroy(struct wl_resource* resource)
 {
-    //TODO: clean up resource
-
     wl_list_remove(wl_resource_get_link(resource));
 }
 
