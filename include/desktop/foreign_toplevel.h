c90679c0699a99be4862eedf9fb82bc190b50fef
YaEnergy <kiano.van.cauter@gmail.com>
2025-06-14 23:39:07 +0200

    Remove manager

--- include/desktop/foreign_toplevel.h
+++ include/desktop/foreign_toplevel.h
@@ -12,14 +12,6 @@
 
 struct e_view;
 
-struct e_foreign_toplevel_manager
-{
-    struct wlr_foreign_toplevel_manager_v1* wlr_manager;
-    struct wlr_ext_foreign_toplevel_list_v1* ext_list;
-
-    struct wl_list foreign_toplevels; //struct e_foreign_toplevel*
-};
-
 struct e_foreign_toplevel
 {
     struct e_view* view;
@@ -37,12 +29,9 @@ struct e_foreign_toplevel
 
     struct wl_listener wlr_destroy;
     struct wl_listener ext_detroy;
-
-    struct wl_list link; //e_foreign_toplevel_manager::foreign_toplevels
 };
 
-bool e_foreign_toplevel_management_init(struct wl_display* display);
-
-struct e_foreign_toplevel* e_foreign_toplevel_create(struct e_foreign_toplevel_manager* manager, struct e_view* view);
+//TODO: views should have access to the server.
+struct e_foreign_toplevel* e_foreign_toplevel_create(struct e_view* view);
 
 void e_foreign_toplevel_destroy(struct e_foreign_toplevel* foreign_toplevel);
