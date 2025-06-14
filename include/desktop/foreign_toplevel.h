1464c6e03488dfe1cb0b09825ec8a4f3def470b6
YaEnergy <kiano.van.cauter@gmail.com>
2025-06-14 23:34:00 +0200

    Use general view

--- include/desktop/foreign_toplevel.h
+++ include/desktop/foreign_toplevel.h
@@ -7,10 +7,10 @@
 #include <wlr/types/wlr_foreign_toplevel_management_v1.h>
 #include <wlr/types/wlr_ext_foreign_toplevel_list_v1.h>
 
-#include "desktop/xdg_shell.h"
-
 //TODO: comments
 //TODO: finish header
+
+struct e_view;
 
 struct e_foreign_toplevel_manager
 {
@@ -22,7 +22,7 @@ struct e_foreign_toplevel_manager
 
 struct e_foreign_toplevel
 {
-    struct e_toplevel_view* toplevel_view;
+    struct e_view* view;
     
     struct wlr_foreign_toplevel_handle_v1* wlr_handle;
     struct wlr_ext_foreign_toplevel_handle_v1* ext_handle;
@@ -34,13 +34,15 @@ struct e_foreign_toplevel
     struct wl_listener request_close;
 
     struct wl_listener set_rectangle;
-    struct wl_listener destroy;
+
+    struct wl_listener wlr_destroy;
+    struct wl_listener ext_detroy;
 
     struct wl_list link; //e_foreign_toplevel_manager::foreign_toplevels
 };
 
 bool e_foreign_toplevel_management_init(struct wl_display* display);
 
-struct e_foreign_toplevel* e_foreign_toplevel_create(struct e_foreign_toplevel_manager* manager, struct e_toplevel_view* toplevel_view);
+struct e_foreign_toplevel* e_foreign_toplevel_create(struct e_foreign_toplevel_manager* manager, struct e_view* view);
 
 void e_foreign_toplevel_destroy(struct e_foreign_toplevel* foreign_toplevel);
