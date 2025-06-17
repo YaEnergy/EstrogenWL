7ea393e38517a0c4bf10dc925a550244502cbfd0
Kiara <kiano.van.cauter@gmail.com>
2025-06-17 19:45:29 +0200

    Log improvements (#53)
    * Truncate log message if necessary
    
    * Don't quit on log file fail
    
    * Rename logFile to log_file
    
    * Update get_time_string
    
    * Custom wlr log handler
    
    * Use same handler for info & error log
    
    * Remove unused header
    
    * Use correct importance name in file
    
    * Remove log file, rename e_log_vlog to e_vlog
    
    * Check verbosity level
    
    * Colors
    
    * Change log int msg

--- include/util/log.h
+++ include/util/log.h
@@ -1,9 +1,8 @@
 #pragma once
 
-int e_log_init(void);
+// Init wlr log & log file.
+void e_log_init(void);
 
-void e_log_info(const char* format, ...);
+void e_log_info(const char* fmt, ...);
 
-void e_log_error(const char* format, ...);
-
-void e_log_fini(void);
+void e_log_error(const char* fmt, ...);
